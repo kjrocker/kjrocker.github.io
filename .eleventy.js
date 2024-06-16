6bb1b485112b4760c859a65ed3ceb5d384a66459
Kevin Rocker <kevin.j.rocker@gmail.com>
2024-06-16 02:52:42 -0700

    Mobile UI improvements, minification

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,13 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const { DateTime } = require('luxon');
 const { marked } = require('marked');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const htmlmin = require("html-minifier-terser");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/assets/css");
 
   eleventyConfig.addFilter("postDate", (date) => {
     return DateTime.fromJSDate(date, {zone: 'utc'}).toLocaleString(DateTime.DATE_FULL);
@@ -16,6 +17,18 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("markdownify", (data) => {
     return marked.parse(data)
+  });
+
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      return htmlmin.minify(content, {
+        collapseWhitespace: true,
+        removeComments: true,
+        useShortDoctype: true
+      });
+    }
+
+    return content;
   });
 
   return {
