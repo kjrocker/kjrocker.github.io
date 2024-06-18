67696234e816601ccb78086e30cae708e7a30170
Kevin Rocker <kevin.j.rocker@gmail.com>
2024-06-17 20:30:44 -0700

    RSS plugin, upgrading to v3 out of sheer annoyance

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,25 @@
 
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
-const { DateTime } = require('luxon');
-const { marked } = require('marked');
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const htmlmin = require("html-minifier-terser");
-const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+// const { EleventyRenderPlugin } = require("@11ty/eleventy");
+// const { DateTime } = require('luxon');
+// const { marked } = require('marked');
+// const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+// const htmlmin = require("html-minifier-terser");
+// const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+// const rss = require("@11ty/eleventy-plugin-rss");
 
-module.exports = function (eleventyConfig) {
+import { EleventyRenderPlugin } from "@11ty/eleventy";
+import { DateTime } from 'luxon';
+import { marked } from 'marked';
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import htmlmin from "html-minifier-terser";
+import pluginSitemap from "@quasibit/eleventy-plugin-sitemap";
+import rss from "@11ty/eleventy-plugin-rss";
+
+export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginSitemap, {
-    hostname: 'https://www.kevinrocker.com',
+    hostname: 'https://kevinrocker.com',
 });
 
   eleventyConfig.addPassthroughCopy("src/assets/css");
@@ -34,6 +43,28 @@ module.exports = function (eleventyConfig) {
 
     return content;
   });
+
+  // eleventyConfig.addPlugin(rss);
+  // eleventyConfig.addFilter("absoluteUrl", rss.absoluteUrl);
+  // eleventyConfig.addFilter("htmlToAbsoluteUrls", rss.htmlToAbsoluteUrls);
+  eleventyConfig.addPlugin(rss.feedPlugin, {
+		type: "rss", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "blog", // iterate over `collections.posts`
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Kevin Rocker's Blog",
+			subtitle: "Musings about technology, software development, and miscellenia.",
+			base: "https://kevinrocker.com/",
+			author: {
+				name: "Kevin Rocker",
+				email: "", // Optional
+			}
+		}
+	});
 
   return {
     passthroughFileCopy: true,
